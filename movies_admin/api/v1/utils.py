e6a287ea8f9bb3078a342f5675233157008baa88
mechnotech <mechnotech@yandex.ru>
2021-10-25 15:46:04 +0300

    рефакторинг MoviesApiMixin

--- movies_admin/api/v1/utils.py
+++ movies_admin/api/v1/utils.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.http import JsonResponse
 
 from django.conf import settings
-from movies.models import Filmwork
+from movies.models import Filmwork, RoleType
 
 
 class MoviesApiMixin:
@@ -11,16 +11,18 @@ class MoviesApiMixin:
     http_method_names = ['get']
     paginate_by = settings.API_MOVIES_PER_PAGE
 
+    def _aggregate_person(self, role):
+        return ArrayAgg('personrole__person_id__full_name', distinct=True, filter=Q(personrole__role=role))
+
     def get_queryset(self):
         qs = super(MoviesApiMixin, self).get_queryset()
         qs = qs.prefetch_related('genres', 'persons')
         qs = qs.values('id', 'title', 'description', 'creation_date', 'rating', 'type')
         qs = qs.annotate(
             genres=ArrayAgg('filmworkgenre__genre_id__name', distinct=True),
-            actors=ArrayAgg('personrole__person_id__full_name', distinct=True, filter=Q(personrole__role='actor')),
-            directors=ArrayAgg('personrole__person_id__full_name', distinct=True,
-                               filter=Q(personrole__role='director')),
-            writers=ArrayAgg('personrole__person_id__full_name', distinct=True, filter=Q(personrole__role='writer')),
+            actors=self._aggregate_person(RoleType.ACTOR),
+            directors=self._aggregate_person(RoleType.DIRECTOR),
+            writers=self._aggregate_person(RoleType.WRITER)
         )
         return qs
 
